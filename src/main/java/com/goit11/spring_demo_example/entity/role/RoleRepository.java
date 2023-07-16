d3b13ad08c7b7af1cb030c1180945d88b1fb13a2
syvolapenko <isivolap@gmail.com>
2023-07-16 12:36:06 +0300

    create complete test

--- src/main/java/com/goit11/spring_demo_example/entity/role/RoleRepository.java
+++ src/main/java/com/goit11/spring_demo_example/entity/role/RoleRepository.java
@@ -1,6 +1,8 @@
 package com.goit11.spring_demo_example.entity.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
